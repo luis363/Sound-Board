9d5ce608cf2251900db00887de11c96ceb24e69f
SickoMode_Luis <luis363coc@gmail.com>
2022-12-11 16:50:11 -0500

    Add files via upload

---
+++ css and js/index.js
@@ -0,0 +1,41 @@
+var audio = new Audio();
+
+function btn1(){
+    audio.src = "sounds/vine-dramatic-boom-sound-effect.mp3";
+    audio.play();
+}
+
+function btn2(){
+    audio.src = "sounds/scream1.mp3";
+    audio.play();
+}
+
+function btn3(){
+    audio.src = "sounds/rizz-sounds.mp3";
+    audio.play();
+}
+
+function btn4(){
+    audio.src = "sounds/lightskin-stare.mp3";
+    audio.play();
+}
+
+function btn5(){
+    audio.src = "sounds/dry-fart.mp3";
+    audio.play();
+}
+
+function btn6(){
+    audio.src = "sounds/scooby-doo-laugh-sped-up.mp3";
+    audio.play();
+}
+
+function btn7(){
+    audio.src = "sounds/giga-chad.mp3";
+    audio.play();
+}
+
+function btn8(){
+    audio.src = "sounds/anime-wow-sound-effect.mp3";
+    audio.play();
+}
